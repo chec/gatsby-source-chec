cd75f97a162ef44ce7680e6f9587ee207384e519
Jamie Barton <jamie@notrab.dev>
2020-08-28 20:02:26 +0100

    chore: set default publicKey

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -6,7 +6,9 @@ module.exports = {
     {
       resolve: '@chec/gatsby-source-chec',
       options: {
-        publicKey: process.env.CHEC_PUBLIC_KEY,
+        publicKey:
+          process.env.CHEC_PUBLIC_KEY ||
+          'pk_184625ed86f36703d7d233bcf6d519a4f9398f20048ec',
         downloadImageAssets: true,
       },
     },
