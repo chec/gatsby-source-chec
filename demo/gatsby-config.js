aba690ee5600d1197e6e643dd4b55e838dfab159
Jamie Barton <jamie@notrab.dev>
2020-08-28 19:36:16 +0100

    cherrypick

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 
 module.exports = {
   plugins: [
+    'gatsby-plugin-sharp',
     {
       resolve: '@chec/gatsby-source-chec',
       options: {
         publicKey: process.env.CHEC_PUBLIC_KEY,
+        downloadImageAssets: true,
       },
     },
+    'gatsby-transformer-sharp',
   ],
 };
--- gatsby-source-chec/gatsby-node.js
+++ gatsby-source-chec/gatsby-node.js
@@ -1,2 +1,3 @@
 exports.sourceNodes = require('./gatsby/node/sourceNodes');
 exports.createSchemaCustomization = require('./gatsby/node/createSchemaCustomization');
+exports.onCreateNode = require('./gatsby/node/onCreateNode');
